b69de36e17365869d83403fb372910bf4a923f45
maddie-j <no45483361@hotmail.com>
2024-03-08 16:26:30 +1100

    Fixing crash on game startup

--- src/main/java/com/eerussianguy/blazemap/mixin/ChunkHolderMixin.java
+++ src/main/java/com/eerussianguy/blazemap/mixin/ChunkHolderMixin.java
@@ -15,13 +15,14 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ChunkHolder.class)
 public class ChunkHolderMixin {
-    @Shadow private boolean hasChangedSections;
-    @Shadow @Final private BitSet skyChangedLightSectionFilter;
-    @Shadow @Final private BitSet blockChangedLightSectionFilter;
+    // @Shadow private boolean hasChangedSections;
+    // @Shadow @Final private BitSet skyChangedLightSectionFilter;
+    // @Shadow @Final private BitSet blockChangedLightSectionFilter;
 
     @Inject(method = "broadcastChanges", at = @At("HEAD"))
     private void broadcastChanges(LevelChunk chunk, CallbackInfo ci) {
-        if(this.hasChangedSections || !this.skyChangedLightSectionFilter.isEmpty() || !this.blockChangedLightSectionFilter.isEmpty()) {
+        // if(this.hasChangedSections || !this.skyChangedLightSectionFilter.isEmpty() || !this.blockChangedLightSectionFilter.isEmpty()) {
+        if(true) {
             BlazeMapServerEngine.onChunkChanged(chunk.getLevel().dimension(), chunk.getPos());
         }
     }
