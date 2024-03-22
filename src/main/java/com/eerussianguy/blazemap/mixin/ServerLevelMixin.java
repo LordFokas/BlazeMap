4ff55ccd00d3d97bb7cb883fb148fbaef8d917f5
LordFokas <LordFokas@users.noreply.github.com>
2024-03-22 09:21:55 +0000

    Fix issue with BME-9 fix that also brought the same pain as BME-40.

--- src/main/java/com/eerussianguy/blazemap/mixin/ServerLevelMixin.java
+++ src/main/java/com/eerussianguy/blazemap/mixin/ServerLevelMixin.java
@@ -7,17 +7,20 @@ import net.minecraft.world.level.block.state.BlockState;
 
 import com.eerussianguy.blazemap.engine.server.BlazeMapServerEngine;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ServerLevel.class)
 public abstract class ServerLevelMixin {
-    @Shadow public abstract ServerLevel getLevel();
 
     @Inject(method = "onBlockStateChange", at=@At("HEAD"))
     public void onBlockStateChange(BlockPos block, BlockState s1, BlockState s2, CallbackInfo ci) {
-        BlazeMapServerEngine.onChunkChanged(this.getLevel().dimension(), new ChunkPos(block));
+        // If it's stupid but it works, it's not stupid.
+        // @Shadow fucks with the project because it doesn't reobf properly.
+        // No one knows why, no one can / wants to help, and so no one can point fingers at us either.
+        ServerLevel level = (ServerLevel) (Object) this;
+
+        BlazeMapServerEngine.onChunkChanged(level.dimension(), new ChunkPos(block));
     }
 }
