64177d12780ad929638b155f224cc4fedbbe97b7
LordFokas <LordFokas@users.noreply.github.com>
2024-04-01 18:07:29 +0100

    BME-27: Resizable minimap

--- src/main/java/com/eerussianguy/blazemap/feature/maps/MinimapWidget.java
+++ src/main/java/com/eerussianguy/blazemap/feature/maps/MinimapWidget.java
@@ -12,6 +12,8 @@ import com.mojang.blaze3d.platform.Window;
 import com.mojang.blaze3d.vertex.PoseStack;
 
 public class MinimapWidget {
+    private static final int HANDLE_SIZE = 30;
+
     private final MinimapConfigSynchronizer synchronizer;
     private final ClientConfig.MinimapConfig config = BlazeMapConfig.CLIENT.minimap;
     private final MapRenderer map;
@@ -24,14 +26,21 @@ public class MinimapWidget {
     }
 
     public void render(PoseStack stack, MultiBufferSource buffers) {
+        int width = config.width.get();
+        int height = config.height.get();
         stack.translate(config.positionX.get(), config.positionY.get(), 0);
 
         stack.pushPose();
         stack.translate(-5, -5, 0);
-        RenderHelper.fillRect(stack.last().pose(), config.width.get() + 10, config.height.get() + 10, Colors.WIDGET_BACKGROUND);
+        RenderHelper.fillRect(stack.last().pose(), width + 10, height + 10, Colors.WIDGET_BACKGROUND);
         stack.popPose();
 
         map.render(stack, buffers);
+
+        if(editor){
+            stack.translate(width - HANDLE_SIZE, height - HANDLE_SIZE, 0);
+            RenderHelper.fillRect(buffers, stack.last().pose(), HANDLE_SIZE, HANDLE_SIZE, 0xFFFF0000);
+        }
     }
 
     public boolean mouseDragged(double cx, double cy, int button, double dx, double dy) {
@@ -40,19 +49,28 @@ public class MinimapWidget {
         int sx = config.width.get();
         int sy = config.height.get();
 
+        // Check if mouse is within minimap bounds
         if(cx < px || cy < py || cx > px + sx || cy > py + sy){
             return false;
         }
 
+        // Calculate window relative bounds
         Window window = Minecraft.getInstance().getWindow();
+        int maxX = window.getWidth() - (px+sx);
+        int maxY = window.getHeight() - (py+sy);
 
-        int minX = -px, maxX = window.getWidth() - (px+sx);
-        int minY = -py, maxY = window.getHeight() - (py+sy);
-        int mx = Helpers.clamp(minX, (int) dx, maxX);
-        int my = Helpers.clamp(minY, (int) dy, maxY);
-
-        synchronizer.setPosition(px + mx, py + my);
-
+        // Check if outside handle bounds
+        if(cx < px + sx - HANDLE_SIZE || cy < py + sy - HANDLE_SIZE){
+            // Move map
+            int mx = Helpers.clamp(-px, (int) dx, maxX);
+            int my = Helpers.clamp(-py, (int) dy, maxY);
+            synchronizer.setPosition(px + mx, py + my);
+        }else{
+            // Resize map
+            int rx = Helpers.clamp(-sx, (int) dx, maxX);
+            int ry = Helpers.clamp(-sy, (int) dy, maxY);
+            synchronizer.setSize(sx + rx, sy + ry);
+        }
         return true;
     }
 }
