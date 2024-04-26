17ca8a820e015a359957880a65595e04a1a094c7
maddie-j <no45483361@hotmail.com>
2024-04-27 01:03:39 +1000

    BME-85 Made Sodium compat more efficient (fires less often when unnecessary)

--- src/main/java/com/eerussianguy/blazemap/mixin/SodiumCompatMixin.java
+++ src/main/java/com/eerussianguy/blazemap/mixin/SodiumCompatMixin.java
@@ -3,23 +3,21 @@ package com.eerussianguy.blazemap.mixin;
 import com.eerussianguy.blazemap.engine.client.BlazeMapClientEngine;
 import com.eerussianguy.blazemap.feature.MDSources;
 import com.eerussianguy.blazemap.profiling.Profilers;
-import me.jellysquid.mods.sodium.client.render.chunk.lists.ChunkRenderList;
 import net.minecraft.world.level.ChunkPos;
-import me.jellysquid.mods.sodium.client.render.chunk.RenderSection;
+import me.jellysquid.mods.sodium.client.render.SodiumWorldRenderer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(ChunkRenderList.class)
+@Mixin(SodiumWorldRenderer.class)
 public class SodiumCompatMixin {
-
-    @Inject(method = "add", at = @At("HEAD"), remap = false)
-    void onAdd(RenderSection render, CallbackInfo ci) {
+    @Inject(method = "scheduleRebuildForChunk", at = @At("HEAD"), remap = false)
+    void onScheduleRebuildForChunk(int x, int y, int z, boolean important, CallbackInfo ci) {
         Profilers.Client.Mixin.SODIUM_LOAD_PROFILER.hit();
         Profilers.Client.Mixin.SODIUM_TIME_PROFILER.begin();
 
-        BlazeMapClientEngine.onChunkChanged(new ChunkPos(render.getChunkX(), render.getChunkZ()), MDSources.Client.SODIUM);
+        BlazeMapClientEngine.onChunkChanged(new ChunkPos(x, z), MDSources.Client.SODIUM);
 
         Profilers.Client.Mixin.SODIUM_TIME_PROFILER.end();
     }
